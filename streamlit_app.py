8e7c9dd16ddfbc72edeb6107900ab179ac2f46e6
mikelcampo <98816881+mikelcampo@users.noreply.github.com>
2022-05-06 10:16:25 +0200

    Update streamlit_app.py
    json no longer display

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
                                    
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
